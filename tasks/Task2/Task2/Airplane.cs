bd555fc6e3f4ef92059bd373bccc119aeb3eed6d
Rene Haas <ic15b027@technikum-wien.at>
2016-05-02 18:03:59 +0200

    Task 2 completed

---
+++ tasks/Task2/Task2/Airplane.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Airplane
+    {
+        private double m_fuel_level;
+
+        public double Wingspan { get; set; }
+        public double Length { get; set; }
+
+        public Airplane(double fuel_level, double wingspan, double length)
+        {
+            Refuel(fuel_level);
+            Wingspan = wingspan;
+            Length = length;
+        }
+
+
+        public double GetFuelLevel()
+        {
+            return m_fuel_level;
+        }
+
+        public void Refuel(double fuel_amount)
+        {
+            m_fuel_level = fuel_amount;
+        }
+    }
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Airplane[] planes = new Airplane[]
+            {
+                new Airplane(12345.67, 67.33, 52.41),
+                new Airplane(23456.78, 78.44, 63.51),
+                new Airplane(34567.89, 89.55, 74.61),
+            };
+            foreach (var a in planes)
+            {
+                Console.WriteLine($"Airlane is {a.Length}m long, has a wingspan of {a.Wingspan}m and is filled with {a.GetFuelLevel()}l of fuel.");
+            }
+            Console.WriteLine("________________________________________");
+            planes[0].Refuel(10000);
+            planes[1].Refuel(20000);
+            planes[2].Refuel(30000);
+            foreach (var a in planes)
+            {
+                Console.WriteLine($"Airlane is {a.Length}m long, has a wingspan of {a.Wingspan}m and is filled with {a.GetFuelLevel()}l of fuel.");
+            }
+        }
+    }
+}
